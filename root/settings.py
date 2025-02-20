1d6cb0a964f424d6bd7d2232547359e7c630b765
qahramonovas <sarvinozqahramonova66@gmail.com>
2025-02-20 16:55:02 +0500

    ci cd

--- root/settings.py
+++ root/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*37qhz1ize!x@2v$rec$ig93zm^q6c2a!=&kzzt4ih7@w6^z*l'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.auth',
