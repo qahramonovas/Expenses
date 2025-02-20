f23911fc506ece35ef9f6c36c736ef8401967f00
qahramonovas <sarvinozqahramonova66@gmail.com>
2025-02-20 16:56:05 +0500

    ci cd

--- root/settings.py
+++ root/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Your Project API',
+    'TITLE': 'Sarvinoz Project API',
     'DESCRIPTION': 'Your project description',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
